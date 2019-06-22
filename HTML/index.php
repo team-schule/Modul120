8ee1787d45d39a8f2e93691041e176c45a64f41d
davidzweili <david.zweili@gmail.com>
2019-06-22 15:26:45 +0200

    Index Login und Kategorien erweitert

--- HTML/index.php
+++ HTML/index.php
@@ -2,6 +2,8 @@
 
 <?php
     session_start();
+    include("db.inc.php");
+    verbindung_mysql("Modul120")
 ?>
 
 <html lang="en" dir="ltr">
@@ -18,15 +20,54 @@
     <h1>Test Seite Projekt Modul120</h1>
 
     <?php
+    
+    //********************************************************************************************************
+    // Login / Anmeldung
+    
     if (isset($_SESSION["userData"]))
     {
         $userData = $_SESSION["userData"];
-        echo "Hallo " . $userData["VORNAME"] . " du hast " . $userData["TOTAL_INSERATE"] . " Inserate am laufen";
+        
+        echo
+        "<li><a href='#'>" . $userData["LOGIN_UID"] . "</a>
+            <ul>
+                <li><a href='#'>Meine Artikel</a></li>
+                <li><a href='#'>Wunschliste</a></li>
+                <li><a href='#'>Blabla</a></li>
+                <li><a href='#'>Abmelden</a></li>
+            </ul>
+        </li>";
     }
     else
     {
         echo "<input type=button onClick=\"location.href='login.php'\" value='Login'>";
     }
+    //********************************************************************************************************
+    // Suche
+    
+    
+    //********************************************************************************************************
+    // Kategorien
+    
+    $sql =
+    "SELECT * ".
+    "FROM kategorien";
+
+    $result = mysql_query ($sql);
+
+
+    $kategorienRows = array();
+
+    while(($kategorienRows = mysql_fetch_array($result)))
+    {
+        $rows[] = $kategorienRows;
+    }
+    
+    foreach($rows as $key=> $item)
+    {
+        echo "<li><a href='#'>" . $rows[$key]["KATEGORIE_TEXT"] . "</a></li>";
+    }
+    //********************************************************************************************************
     
     ?>
 
