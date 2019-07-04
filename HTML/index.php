079c2e5f4d4ebd2bdf3812ae563483e03aaecb87
R34perCH <p.tomasi@hotmail.ch>
2019-07-04 15:25:50 +0200

    Inserate Anzeige etwas angepasst und es werden nur immer 12 Inserate Angezeigt im Random Modus

--- HTML/index.php
+++ HTML/index.php
@@ -16,11 +16,20 @@
 
     .anzeigefeld>div {
         width: 300px;
-        height: auto;
+        height: 500px;
         background-color: rgba(255, 255, 255, 0.8);
         border: 1px solid black;
         text-align: center;
         font-size: 20px;
+        border-radius: 10px;
+    }
+    .anzeigefeld p{
+        margin: 10px;
+    }
+    .anzeigefeld input[type=button]{
+      margin: 0 auto;
+      display: block;
+
     }
 
     .bildinserat {
@@ -144,104 +153,104 @@ echo "<div class='anzeigefeld'>";
     ?>
 
 <?php
-$sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate";
+$sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate ORDER BY RAND() LIMIT 12";
 if(isset($_REQUEST_['Alle Kategorien'])){
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate ORDER BY RAND() LIMIT 12";
 }
 elseif(isset($_REQUEST['Antiquitäten'])){
   $meldung = "Antiquitäten";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=1";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=1 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['TV,Video'])) {
   $meldung = "TV,Video";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=2";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=2 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Briefmarken'])) {
   $meldung = "Briefmarken";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=3";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=3 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Bücher'])) {
   $meldung = "Bücher";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=4";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=4 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Computer'])) {
   $meldung = "Computer";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=5";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=5 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Filme,DVD'])) {
   $meldung = "Filme,DVD";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=6";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=6 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Spielkonsolen'])) {
   $meldung = "Spielkonsolen";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=7";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=7 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Smartphones'])) {
   $meldung = "Smartphones";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=8";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=8 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Haushalt,Wohnen'])) {
   $meldung = "Haushalt,Wohnen";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=9";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=9 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Kleidung'])) {
   $meldung = "Kleidung";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=10";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=10 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Freizeit,Hobby'])) {
   $meldung = "Freizeit,Hobby";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=11";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=11 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Musikinstrumente'])) {
   $meldung = "Musikinstrumente";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=12";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=12 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Münzen'])) {
   $meldung = "Münzen";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=13";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=13 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Sammeln,Seltenes'])) {
   $meldung = "Sammeln,Seltenes";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=14";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=14 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Spielzeug'])) {
   $meldung = "Spielzeug";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=15";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=15 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Sport'])) {
   $meldung = "Sport";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=16";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=16 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Tierzubehör'])) {
   $meldung = "Tierzubehör";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=17";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=17 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Schmuck'])) {
   $meldung = "Schmuck";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=18";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=18 ORDER BY RAND() LIMIT 12";
 }
 elseif (isset($_REQUEST['Wein,Genus'])) {
   $meldung = "Wein,Genus";
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=19";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where KATEGORIE_ID=19 ORDER BY RAND() LIMIT 12";
 }
 elseif(isset($_POST['suchen'])){
   $suche = $_POST['usereingabe'];
-  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where TITEL like '%$suche%'";
+  $sql = "SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate where TITEL like '%$suche%' ORDER BY RAND() LIMIT 12";
   $meldung = $suche;
   }
   else {
-    $sql="SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate";
+    $sql="SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate ORDER BY RAND() LIMIT 12";
   }
 
 $erg = mysql_query($sql);
 if(mysql_num_rows($erg)==0){
   if(isset($_POST['suchen'])){
     echo "<script>alert('Keine Treffer für die suche $meldung ')</script>";
-    $sql="SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate";
+    $sql="SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate ORDER BY RAND() LIMIT 12";
   }
   else {
     echo "<script>alert('Keine Inserate in der Kategorie $meldung gefunden')</script>";
-    $sql="SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate";
+    $sql="SELECT TITEL, BILD, INHALT, ERFASST_AM, PREIS_START, ANGEZEIGT_BIS, INSERATE_ID from inserate ORDER BY RAND() LIMIT 12";
   }
 }
 $abfrage = mysql_query($sql);
@@ -274,7 +283,7 @@ if( ! $abfrage)
                     <p style ='font-size:0.8em'>$bild </p>
 
                     <p style='font-size:0.7em'>" .$zeile["INHALT"] ."</p>
-                    <p style='color:red'>" .$zeile["PREIS_START"] ." CHF</p>
+                    <p style='color:red'><b>" .$zeile["PREIS_START"] ." CHF</b></p>
 
                     <p style='font-size:0.9em'>Endet am: $dateformat   </p>
                     <p><input type=button onClick=\"location.href='inserat.php?id=" . $zeile["INSERATE_ID"] . "'\" value='Zum Inserat'></input></p>
